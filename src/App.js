2bb30116f30307cb5f32898104b3c9a4c1c73428
sarthakKarode <sarthakkarode@gmail.com>
2024-11-24 02:20:27 +0530

    Initial home 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React,{useState} from "react";
 import { BsBrightnessHigh } from "react-icons/bs";
 import { BsBrightnessHighFill } from "react-icons/bs";
+import Profile from './IMG_5436.jpeg'
+import { MdArrowOutward } from "react-icons/md";
 import './App.css'
 
 const App=()=>{
@@ -21,6 +23,14 @@ const App=()=>{
           {theme ? <BsBrightnessHigh onClick={clickTheme} className="headerLogo"/> : <BsBrightnessHighFill onClick={clickTheme} className="headerLogo"/>}
           <div className="headerBtn">Contact Me</div>
         </div>
+      </div>
+      <div className="home">
+        <div className="homeHeading1">Hi, I'm <div className="homeHeading1Name">&nbsp; Sarthak </div></div>
+        <div className="homeImageBox">
+          <div className="homeHeading">Full-Stack Developer</div>
+          <img className="homeImage" src={Profile} alt="MyImage"/>
+        </div>
+          <div className="homeBtn">Connect with me</div>
       </div>
     </div>
   )
